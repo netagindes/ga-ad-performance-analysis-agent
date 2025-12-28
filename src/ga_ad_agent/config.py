c819c48cae56f4de4c0f1cccab622dffe5fe1251
netagindes <neta.gindes@gmail.com>
2025-12-28 23:33:46 +0200

    config.py update

--- src/ga_ad_agent/config.py
+++ src/ga_ad_agent/config.py
@@ -3,34 +3,54 @@ from pathlib import Path
 from dotenv import load_dotenv
 
 
-ENV_PATH = Path(__file__).resolve().parent / ".env"
+PROJECT_ROOT = Path(__file__).resolve().parents[2]
+ENV_PATH = PROJECT_ROOT / ".env"
 
-if ENV_PATH.exists():
+if ENV_PATH.exists():  # Load .env file from project root
     load_dotenv(dotenv_path=ENV_PATH)
 else:
-    # .env is optional; this is normal in CI / production
+    # Environment variables should have been set using the terminal export commands
     pass
 
 
-def fetch_required_env_var(name: str) -> str:
+def fetch_required_env_var(name: str, default: str = None) -> str:
     value = os.getenv(name)
+
     if not value:
+        if default:
+            return default
+
         raise RuntimeError(
             f"Missing required environment variable: {name}\n"
-            f"Did you forget to set it or create a .env file?"
         )
     return value
 
 
-# OPENAI_API_KEY: str = fetch_required_env_var("OPENAI_API_KEY")
-GOOGLE_CLOUD_PROJECT: str = os.getenv("GOOGLE_CLOUD_PROJECT", "ad-performance-analysis-agent")
+# Fetch required environment variables
+GOOGLE_CLOUD_PROJECT: str = fetch_required_env_var("GOOGLE_CLOUD_PROJECT")
+GEMINI_API_KEY: str = fetch_required_env_var("GEMINI_API_KEY")
+GOOGLE_API_KEY: str = fetch_required_env_var("GOOGLE_API_KEY")
+REGION: str = fetch_required_env_var("REGION", "US")
 
 
 def main():
-    # print(f"OPENAI_API_KEY: {OPENAI_API_KEY}")
-    # print(f"GOOGLE_CLOUD_PROJECT: {GOOGLE_CLOUD_PROJECT}")
+
+    if not all([GOOGLE_CLOUD_PROJECT, GEMINI_API_KEY, GOOGLE_API_KEY]): # Check if all required environment variables are set
+        print("Missing required environment settings")
+
+        if not ENV_PATH.exists():
+            raise RuntimeError(
+                f"""Missing required environment settings - 
+                Go over the instructions in step [1] of the README.md file."""
+            )
+        
     return
 
 
 if __name__ == "__main__":
     main()
+
+    # print(f"GOOGLE_CLOUD_PROJECT: {GOOGLE_CLOUD_PROJECT}")
+    # print(f"GEMINI_API_KEY: {GEMINI_API_KEY}")
+    # print(f"GOOGLE_API_KEY: {GOOGLE_API_KEY}")
+    # print(f"REGION: {REGION}")
